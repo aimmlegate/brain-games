ce58beedae3889bbf3f2982958acb18a4028bfe3
root <root@DESKTOP-JDG6322.localdomain>
2018-03-07 17:51:45 +0500

    add gcd game

---
+++ src/games/gcdGame.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+import gameEngine from '..';
+
+const euclid = (n, m) => {
+  if (n === m) return n;
+  return (n < m) ? euclid(n, m - n) : euclid(m, n - m);
+};
+
+const gameLogic = () => {
+  const startMessage = 'Find the greatest common divisor of given numbers.';
+  const num1 = Math.floor(Math.random() * (50 - 1)) + 1;
+  const num2 = Math.floor(Math.random() * (50 - 1)) + 1;
+  const question = `${num1} ${num2}`;
+  const rightAnswer = euclid(num1, num2);
+  return (key:string) => {
+    switch (key) {
+      case 'startMessage': return startMessage;
+      case 'question': return question;
+      case 'rightAnswer': return rightAnswer.toString();
+      default: return null;
+    }
+  };
+};
+
+export default () => gameEngine(gameLogic);
